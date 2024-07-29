126fa2d893c6e704b2b8a29884d8e1a231d21666
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-07-29 17:36:16 +0530

    migration setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next"
+import { IBM_Plex_Mono } from "next/font/google"
+
+interface LayoutProps {
+    children: React.ReactNode
+}
+
+const ibmplexmono = IBM_Plex_Mono({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700"]
+});
+
+export const metadata: Metadata = {
+    title: "Siddharth",
+    description: "My Portfolio Website"
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+
+
+export default function Page() {
+    return (
+        <>helo</>
+    );
+}
