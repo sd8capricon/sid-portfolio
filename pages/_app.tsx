b458488b3ad1a6ca3e5bde1a81e9fc1c3475246c
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-07-30 11:45:07 +0530

    removed unused spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Router from 'next/router';
 import { useState } from 'react';
-import Loader from '../components/Loader';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
@@ -10,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   Router.events.on('routeChangeComplete', () => setLoading(false));
   return (
     <>
-      {loading ? <Loader /> : <Component {...pageProps} />}
+      <Component {...pageProps} />
     </>
   )
 }
