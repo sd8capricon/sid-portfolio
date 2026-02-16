960850c8cf58e1afaa82269a7a55615706bf7a89
Siddharth Dhaigude <sd8capricon@gmail.com>
2026-02-16 22:57:20 +0530

    refactor resume data keys

--- utils/resume.ts
+++ utils/resume.ts
@@ -1,16 +1,16 @@
 import { ResumeItemInterface } from "../types"
 
-interface ServerResumeItem extends ResumeItemInterface {
-    list: string[];
-    additional_lists?: {
+interface ResumeEntry extends ResumeItemInterface {
+    highlights: string[];
+    relatedPositions?: {
         position?: string;
-        list: string[]
+        highlights: string[]
     }[]
 }
 
 interface ResumeInterface {
-    left: ServerResumeItem[]
-    right: ServerResumeItem[]
+    left: ResumeEntry[]
+    right: ResumeEntry[]
 }
 
 const Resume: ResumeInterface = {
@@ -19,7 +19,7 @@ const Resume: ResumeInterface = {
             title: "Bachelor Engineering - Computer Engineering",
             subtitle: "Hons in Data Science",
             position: "University of Mumbai(2020-24)",
-            list: [
+            highlights: [
                 "Association of Computing Machinery(ACM) - Vice ChairPerson(2022-23), Asst. Technical Head(2021-22)",
                 "Teknack Gaming Studio - Organizer, Developer & Mentor",
                 "Taught and led technical training sessions, talks and workshops on various tools, technologies and softwares for the students of DBIT as well as other colleges.",
@@ -29,14 +29,14 @@ const Resume: ResumeInterface = {
         {
             title: "Association of Computing Machinery(ACM)",
             position: "Vice Chairperson (2022-23)",
-            list: [
+            highlights: [
                 "Co-ordinate and manage the Core Team and organize events, workshops and trainings for the students.",
                 "Manage and coordinate all the activities of the chapter and responsible for all administrative, functional and Organizational duties in the chapter."
             ],
-            additional_lists: [
+            relatedPositions: [
                 {
                     position: "Asst. Technical Head (2021-22)",
-                    list: [
+                    highlights: [
                         "Conducted workshops, taught and trained students on topics like Git, GitHub, Web Development.",
                         "Responsible for maintaining and implementing new features for the offical chapter website."
                     ]
@@ -49,7 +49,7 @@ const Resume: ResumeInterface = {
             title: "Sacred Hearts School",
             position: "Website Developer",
             link: "https://sacredheartschool.in",
-            list: [
+            highlights: [
                 "Official Website for Sacred Heart School.",
                 "Developed, Designed and Deployed the website",
                 "Technologies: Astro, Tailwind CSS, Typescript"
@@ -59,7 +59,7 @@ const Resume: ResumeInterface = {
             title: "Architect Yogesh Dhaigude",
             position: "Website Developer",
             link: "https://ayda.in",
-            list: [
+            highlights: [
                 "Official Website for Architect Yogesh Dhaigude.",
                 "Developed, Designed and Deployed the website with a content management system.",
                 "Technologies: Astro, Tailwind CSS, Typescript"
@@ -69,7 +69,7 @@ const Resume: ResumeInterface = {
             title: "AKHYA-WOMEN'S GUIDANCE CELL (NGO)",
             position: "Website Developer",
             link: "https://sakhyawgw.org",
-            list: [
+            highlights: [
                 "End-to-end development of the NGO's website, enhancing user experience and engagement.",
                 "Ensured website security by implementing SSL certificates.",
                 "Technologies: Astro, Tailwind CSS, Typescript"
@@ -79,7 +79,7 @@ const Resume: ResumeInterface = {
             title: "Anti-Plastic Brigade Charitable Trust (NGO)",
             position: "Website Developer",
             link: "https://anti-plasticbrigade.com",
-            list: [
+            highlights: [
                 "Official Website for Anti-Plastic Brigade Charitable Trust.",
                 "Developed, Designed and Deployed the website for the NGO.",
                 "Technologies: HTML, CSS, Javascript and Bootstrap."
@@ -88,7 +88,7 @@ const Resume: ResumeInterface = {
         {
             title: "COVID-19 Analysis",
             position: "Data Analysis",
-            list: [
+            highlights: [
                 "Analysed the trends in the covid cases of Andhra Pradesh State, India.",
                 "Applied regression algorithm on Tested vs Confirmed cases, Confirmed vs Recovered cases, Confirmed vs Deceased cases and Confirmed cases vs Timeperiod.",
                 "Technologies: Python, Numpy, Pandas, Matplotlib, Sk-learn"
